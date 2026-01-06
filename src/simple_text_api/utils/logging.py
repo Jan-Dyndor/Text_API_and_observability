5a9b7154ee8db0f96f6c06310990078c49815afa
Jan Dyndor <jandyndor@gmail.com>
2026-01-06 17:11:12 +0100

    loguru middleware

---
+++ src/simple_text_api/utils/logging.py
@@ -0,0 +1,13 @@
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    level="DEBUG",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <7}</level> |id={extra[request_id]}| <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    colorize=True,
+    backtrace=True,
+    diagnose=True,
+    enqueue=True,
+)
